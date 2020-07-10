fae5df013d4f4700e7bb51c9c5678622e3def8e6
Slava Leleka <v.leleka@adguard.com>
2020-07-10 14:38:55 +0300

    try to fix travis

--- build.sh
+++ build.sh
@@ -5,14 +5,13 @@ set -x
 
 yarn install
 
-yarn test
-
 # we do not run browserstack if environment has $TRAVIS variable
 # or if it is pull request from fork $TRAVIS_PULL_REQUEST_SLUG != $TRAVIS_REPO_SLUG
 if [[ ! $TRAVIS || ($TRAVIS && ( $TRAVIS_PULL_REQUEST_SLUG == "$TRAVIS_REPO_SLUG" || $TRAVIS_PULL_REQUEST_SLUG == "" )) ]];
 then
-  yarn global add browserstack-runner
   yarn browserstack
+else 
+  yarn test
 fi
 
 yarn corelibs
